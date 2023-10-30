4a80249f5b8e37a6c61d6fad1365c72d1a7650e7
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-30 16:29:49 +0530

    Update page.js
    Added gradients in between pages

--- app/page.js
+++ app/page.js
@@ -3,16 +3,32 @@ import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World }
 
 const Page = () => (
   <div className='bg-primary-black overflow-hidden'>
+
     <Navbar />
     <Hero />
-    <About />
-    <Explore />
-    <GetStarted />
-    <WhatsNew />
+
+    <div className='relative'>
+      <About />
+        <div className='gradient-03 z-0'/>
+      <Explore />
+    </div>
+
+    <div className='relative'>
+      <GetStarted />
+        <div className='gradient-04 z-0'/>
+      <WhatsNew />
+    </div>
+    
     <World />
-    <Insights />
-    <Feedback />
+
+    <div className='relative'>
+      <Insights />
+        <div className='gradient-04 z-0'/>
+      <Feedback />
+    </div>
+    
     <Footer />
+
   </div>
 );
 
