0c046805f2e09a1be7a41aa2dfe166ace915918e
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-02 20:00:27 +0530

    Update ExploreCard.jsx

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -14,9 +14,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   >
     <img src={imgUrl} alt={title} className="absolute w-full h-full object-cover rounded-[24px]" />
     { active !== id ? (
-      <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] 
-        lg:origin-[0,0]"
-      >        
+      <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">        
         {title}
       </h3>
     ) : (
@@ -24,8 +22,8 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
         <div className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}>
           <img src="/headset.svg" alt="headset" className="w-1/2 h-1/2 object-contain" />
         </div>
-        <p className='font-normal text-[16px] leading-[20px] text-white uppercase'>Enter the Metaverse</p>
-        <h2 className='mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white'>
+        <p className="font-normal text-[16px] leading-[20px] text-white uppercase">Enter the Metaverse</p>
+        <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
           {title}
         </h2>
       </div>
